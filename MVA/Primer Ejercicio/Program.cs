3b81668c469ebafad92c48c29ebf659b3699e97f
Sebas Sebas <seplarui@gmail.com>
2016-10-13 12:12:49 +0200

    WinForms

---
+++ MVA/Primer Ejercicio/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primer_Ejercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            suma s1 = new suma();
+            Double V1=2, V2=2, V3=3, R1;
+            /*  R1 = V1 + V2 + V3;
+              Console.WriteLine("La suma es: " + R1);
+              Console.ReadLine();
+              */
+
+            s1.sumita();
+            Console.ReadLine();
+        }
+
+    }
+    class suma
+    {
+        public void sumita()
+        {
+            Console.WriteLine("Esto es un método de la clase");
+        }
+    }
+}
---
+++ WindowsForm1/WindowsForm1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForm1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x, y, Resultado;
+            x = double.Parse(textBox1.Text);
+            y = double.Parse(textBox2.Text);
+
+            Resultado = x + y;
+            label4.Text = Convert.ToString(Resultado);
+        }
+    }
+}
