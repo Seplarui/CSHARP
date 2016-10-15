be1cd6fe58e33b9b60f9745c1a837f835ff02f26
Sebas Sebas <seplarui@gmail.com>
2016-10-15 13:40:16 +0200

    ProductDatabaseInitializer.cs

--- WingtipToys/WingtipToys/Models/Product.cs
+++ WingtipToys/WingtipToys/Models/Product.cs
@@ -5,7 +5,7 @@ namespace WingtipToys.Models
     public class Product
     {
         [ScaffoldColumn(false)]
-        public int ProductID { get;set }
+        public int ProductID { get; set; }
         [Required, StringLength(100),Display(Name ="Name")]
         public string ProductName { get; set; }
         [Required,StringLength(10000), Display(Name ="Product Description"), DataType(DataType.MultilineText)]
@@ -13,7 +13,7 @@ namespace WingtipToys.Models
         public string ImagePath { get; set; }
         [Display (Name ="Price")]
         public double? UnitPrice { get; set; }
-        public int? CategoryID { get;set }
+        public int? CategoryID { get; set; }
         public virtual Category Category{ get; set; }
     }
 }
